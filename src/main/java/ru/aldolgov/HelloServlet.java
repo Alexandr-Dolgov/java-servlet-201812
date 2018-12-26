b5904ca864a6ce7796a47ba775b38e1785d7c4a2
Alexandr Dolgov <aydolgov@gmail.com>
2018-12-26 09:54:20 +0500

    простейший servlet для запуска в локальном tomcat через idea
    делал по этой статье https://javarush.ru/groups/posts/305-sozdanie-prosteyshego-web-proekta-v-intellij-idea-enterprise-edition-poshagovo-s-kartinkami

---
+++ src/main/java/ru/aldolgov/HelloServlet.java
@@ -0,0 +1,15 @@
+package ru.aldolgov;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello");
+    }
+
+}
