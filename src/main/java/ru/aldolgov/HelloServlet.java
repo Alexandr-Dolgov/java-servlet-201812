fe0de74d5734fca762eab2407e4de21d184567fe
Alexandr Dolgov <aydolgov@gmail.com>
2018-12-26 09:57:09 +0500

    index.html как пример статической страницы
    в отличии от /hello где теперь показывается текущее время сервера

--- src/main/java/ru/aldolgov/HelloServlet.java
+++ src/main/java/ru/aldolgov/HelloServlet.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Date;
 
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("Hello");
+        response.getWriter().println("Hello from " + new Date());
     }
 
 }
