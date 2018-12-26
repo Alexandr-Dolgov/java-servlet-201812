6da429337ed4ef8debbef00851b793652c236163
Alexandr Dolgov <aydolgov@gmail.com>
2018-12-26 10:16:39 +0500

    freemarker: для страниц с данными из сервлета

--- src/main/java/ru/aldolgov/HelloServlet.java
+++ src/main/java/ru/aldolgov/HelloServlet.java
@@ -1,16 +1,39 @@
 package ru.aldolgov;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("Hello from " + new Date());
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+        cfg.setServletContextForTemplateLoading(getServletContext(), "WEB-INF/templates");
+
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setLocale(Locale.US);
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("name", "alexandr");
+        model.put("now", String.valueOf(new Date()));
+
+        Template template = cfg.getTemplate("hello.ftl");
+        try {
+            template.process(model, response.getWriter());
+        } catch (TemplateException ignore) {
+        }
     }
 
 }
---
+++ web/WEB-INF/templates/hello.ftl
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+    <b style="color: green">${name}</b>
+    <b style="color: red">${now}</b>
+</body>
+</html>
